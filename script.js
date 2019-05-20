45c2b06bbec128c6eda24458e8ab68be76d97131
tangojens <47944585+tangojens@users.noreply.github.com>
2019-05-20 11:07:18 +0200

    Jeg har lagt stylehsset ind

--- script.js
+++ script.js
@@ -28,3 +28,17 @@ function showImage(n) {
     dots[i].className = dots[i].className.replace(" active","");
   }
 }
+
+/*video*/
+
+function playPause(btn,vid){
+	var vid = document.getElementById(vid);
+	if(vid.paused){
+		vid.play();
+		btn.innerHTML = "Pause";
+	} else {
+		vid.pause();
+		btn.innerHTML = "Afspil";
+	}
+}
+
