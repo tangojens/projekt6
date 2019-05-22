a2c6419328197c75d9e1c102e78127ba2a503a7b
DaniMichel <danielduvier92@gmail.com>
2019-05-22 18:50:21 +0200

    js
    comment and scroll indicator.

--- script.js
+++ script.js
@@ -41,4 +41,19 @@ function playPause(btn,vid){
 		btn.innerHTML = "Afspil";
 	}
 }
+/*Kommentar sektion*/
+
+
+
+/*Progress Bar*/
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
 
