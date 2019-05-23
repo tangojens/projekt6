59c9272fc26534e5ab40fa58ebbf8073ccb43335
tangojens <47944585+tangojens@users.noreply.github.com>
2019-05-23 11:36:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,12 +33,12 @@ function showImage(n) {
 
 function playPause(btn,vid){
 	var vid = document.getElementById(vid);
-	if(vid.paused){
-		vid.play();
-		btn.innerHTML = "Pause";
-	} else {
-		vid.pause();
-		btn.innerHTML = "Afspil";
+	if(vid.paused){ 
+		vid.play(); 
+		btn.innerHTML = "Pause"; 
+	} else { 
+		vid.pause();  
+		btn.innerHTML = "Afspil"; 
 	}
 }
 
